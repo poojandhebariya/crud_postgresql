3fe5a030a9df79a1ede4d7cdd048e913005ee2e2
poojandhebariya <145736653+poojandhebariya@users.noreply.github.com>
2024-05-28 12:33:15 +0530

    Add project files.

---
+++ crud/Controllers/EmployeeController.cs
@@ -0,0 +1,58 @@
+﻿using crud_postgresql.Models;
+using crud_postgresql.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace crud_postgresql.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class EmployeesController : Controller
+    {
+        private readonly IEmployeeService _employeeService;
+
+        public EmployeesController(IEmployeeService employeeService)
+        {
+            _employeeService = employeeService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            var result = await _employeeService.GetEmployeeList();
+
+            return Ok(result);
+        }
+
+        [HttpGet("{id:int}")]
+        public async Task<IActionResult> GetEmployee(int id)
+        {
+            var result = await _employeeService.GetEmployee(id);
+
+            return Ok(result);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddEmployee([FromBody] Employee employee)
+        {
+            var result = await _employeeService.CreateEmployee(employee);
+
+            return Ok(result);
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> UpdateEmployee([FromBody] Employee employee)
+        {
+            var result = await _employeeService.UpdateEmployee(employee);
+
+            return Ok(result);
+        }
+
+        [HttpDelete("{id:int}")]
+        public async Task<IActionResult> DeleteEmployee(int id)
+        {
+            var result = await _employeeService.DeleteEmployee(id);
+
+            return Ok(result);
+        }
+    }
+}
---
+++ crud/Services/IEmployeeService.cs
@@ -0,0 +1,15 @@
+﻿using crud_postgresql.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace crud_postgresql.Services
+{
+    public interface IEmployeeService
+    {
+        Task<bool> CreateEmployee(Employee employee);
+        Task<List<Employee>> GetEmployeeList();
+        Task<Employee> UpdateEmployee(Employee employee);
+        Task<bool> DeleteEmployee(int key);
+        Task<IActionResult> GetEmployee(int id);
+
+    }
+}
